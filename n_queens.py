d780f6ee0c1266431e0f6ad02a40dadfb84e6b46
praveenchate <praveenxhate5@gmail.com>
2026-05-19 01:04:58 +0530

    Updated project files

--- n_queens.py
+++ n_queens.py
@@ -11,40 +11,31 @@ def print_board(board, n):
 
 def is_safe(board, row, col):
     for i in range(row):
-
         # Check same column
         if board[i] == col:
             return False
-
         # Check left diagonal
         if board[i] - i == col - row:
             return False
-
         # Check right diagonal
         if board[i] + i == col + row:
             return False
-
     return True
 
 
 def solve_n_queen(board, row, n):
-
     # If all queens are placed
     if row == n:
         print_board(board, n)
         return
-
     # Try placing queen in every column
     for col in range(n):
-
         if is_safe(board, row, col):
             board[row] = col
-
             solve_n_queen(board, row + 1, n)
 
 
 n = 4
 board = [-1] * n
-
 print(f"Solutions for {n}-Queen Problem:\n")
 solve_n_queen(board, 0, n)
